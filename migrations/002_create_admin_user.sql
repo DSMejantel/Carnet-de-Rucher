3eefd861560ba5076f3f2764d9fcce0a9685e204
DSMejantel <149260856+DSMejantel@users.noreply.github.com>
2024-02-03 10:13:33 +0100

    Delete migrations directory

--- migrations/002_create_admin_user.sql
+++
@@ -1,4 +0,0 @@
--- Creates an initial user called 'admin'
--- with a password hash that was generated using the 'generate_password_hash.sql' page.
-INSERT INTO user_info (username, activation, nom, prenom, groupe)
-VALUES ('api_admin', '54321', 'Soulié', 'David',3);
