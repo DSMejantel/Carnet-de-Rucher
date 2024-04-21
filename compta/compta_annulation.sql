76afcbecc9a44e0988b557084882b4945712090a
DSMejantel <149260856+DSMejantel@users.noreply.github.com>
2024-04-21 14:32:36 +0200

    Add files via upload

--- compta/compta_annulation.sql
+++ compta/compta_annulation.sql
@@ -3,18 +3,42 @@ SELECT 'redirect' AS component,
  WHERE NOT EXISTS (SELECT 1 FROM login_session WHERE id=sqlpage.cookie('session'));
 SET group_id = (SELECT user_info.groupe FROM login_session join user_info on user_info.username=login_session.username WHERE id = sqlpage.cookie('session'));
 
-SELECT 'redirect' AS component,
-        './/index.sql?restriction' AS link
-        WHERE $group_id<'3';
+--Menu
+SELECT 'dynamic' AS component, sqlpage.read_file_as_text('menu.json') AS properties;
+--Titre
+ select 
+    'title'   as component,
+    'Registre de comptes' as contents,
+    TRUE as center,
+    3         as level;
 
-    -- Mettre à jour l'opération dans la base
-    -- l'opération n'est pas supprimée mais le montant passe à zéro
+select 
+    'alert'                     as component,
+    'Attention !'                   as title,
+    'L''opération ne sera pas effacée totalement de la base. Son montant sera ramené à 0 et l''opération ne sera plus affichée dans le registre. Il restera possible de visualiser les opérations neutralisées sans leur montant initial.' as description,
+    'alert-triangle'            as icon,
+    'orange'                    as color;
     
- UPDATE finances SET prix=0
+select 
+    'title'   as component,
+    'Neutraliser cette opération ?' as contents,
+    TRUE as center,
+    4         as level;    
 
- 
- WHERE id=$id
- RETURNING
-   'redirect' AS component,
-   '../compta.sql?tab=1' as link;
+select 
+    'button' as component,
+    'sm'     as size,
+    'pill'   as shape,
+    'center' as justify;
+select 
+    'Oui' as title,
+    'compta_annulation_confirm.sql?id='||$id as link,
+    'coin-off' as icon,
+    'green' as outline; 
+select 
+    'Non' as title,
+    '../compta.sql?tab=1' as link,
+    'arrow-back-up' as icon,
+    'orange' as outline;
+    
 
